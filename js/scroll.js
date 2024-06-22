43c7974201cff077d454952adfe4bcd669b7a347
MarianoVergniaud <mariano.unla@gmail.com>
2024-06-22 20:26:07 -0300

    quedanimgporcolocarycorrecciones

--- js/scroll.js
+++ js/scroll.js
@@ -2,3 +2,21 @@ function scrollToSection(sectionId) {
     var section = document.getElementById(sectionId);
     section.scrollIntoView({ behavior: 'smooth' });
 }
+
+// deslizar 
+document.addEventListener('DOMContentLoaded', function() {
+    var links = document.querySelectorAll('a.nav-link');
+
+    for (var i = 0; i < links.length; i++) {
+      links[i].addEventListener('click', function(event) {
+        event.preventDefault();
+        var targetId = this.getAttribute('href').substring(1);
+        var targetSection = document.getElementById(targetId);
+
+        window.scrollTo({
+          top: targetSection.offsetTop - 70, // Ajusta el valor para tener en cuenta la altura de la barra de navegación
+          behavior: 'smooth'
+        });
+      });
+    }
+  });
